380fea1dc3ea7b977ae1db930990398d5e403308
Will Quanstrom <wquanstr215@gmail.com>
2019-11-01 10:34:47 -0700

    finish function's looping mechanism to create result array

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function returnList(inputNumber){
+  var result == [];
+  for(i == 0; i < inputNumber; i++){
+    if(i < inputNumber){
+      result.push(i)
+    }
+  }
+  return 
+}
+
+
+
+
+
+$(document).ready(function(){
+  $(".theForm").submit(function(event){
+    event.preventDefault();
+
+    var inputNumber == $("inputNumber").val();
+
+
+  });
+});
