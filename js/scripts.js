6a0fcc4dc8dd952a4ecc48194fddc8bc4638e213
Will Quanstrom <wquanstr215@gmail.com>
2019-11-01 12:31:23 -0700

    add branching to function so that numbers containing 1, 2, or 3 get a text message returned

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,47 @@
+//Business logic
 function returnList(inputNumber){
-
   var result = [];
+
+  if(inputNumber.toString().indexOf('3') >= 0){
+    result.push("I'm sorry, Dave. I'm afraid I can't do that.")
+    return result;
+  } if(inputNumber.toString().indexOf('2') >= 0){
+    result.push("Boop!")
+    return result;
+  } if(inputNumber.toString().indexOf('1') >= 0){
+    result.push("Beep!")
+    return result;
+  } else {
+
   for(i = 0; i < inputNumber; i++){
-    while(i < inputNumber){
-      result += [i];
-      alert(result);
+    while(i <= inputNumber){
+      if([i] == 1){
+        result.push("Beep!")
+      } if([i] == 2){
+        result.push("Boop!")
+      } if([i] == 3){
+        result.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      } else {
+      result.push([i]);
+      }
       i++;
     }
   }
   return result;
-  console.log(result);
+
+}
 }
 
 
 
 
-
+// Front end logic
 $(document).ready(function(){
   $(".theForm").submit(function(event){
     event.preventDefault();
 
     var inputNumber = $("#inputNumber").val();
-    $("#printResult").append(returnList(inputNumber));
+    $("#printResult").text(returnList(inputNumber));
 
 
   });
